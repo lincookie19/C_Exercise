5b2991364a853f762734ecd816748893f83ea14f
lincookie <44064149+lincookie@users.noreply.github.com>
2018-10-21 13:32:56 +0800

    update

---
+++ C_Exercise/6-5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+        int y, z;
+        int i=0;
+        int num;
+        scanf("%d %d %d", &num, &y, &z);
+        if(num==z){
+            i++;
+        }
+        while(i!=y){
+            num++;
+            if(num%z==0){
+                i++;
+            }
+        }
+        printf("%d", num);
+        
+    return 0;
+
+}
