6921ec3b9a097d9acdda62e4059ce6d6d5522ab6
lincookie <44064149+lincookie@users.noreply.github.com>
2018-10-30 13:18:04 +0800

    update

---
+++ C_Exercise/7-3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    char i;
+    int A=0;
+    int B=0;
+    int C=0;
+    int D=0;
+    int E=0;
+    scanf("%c", &i);
+    i=getchar();
+    while(getchar()!=i){
+        if(C=='a' || C=='A'){
+            A++;
+        }
+        if(C=='e' || C=='E'){
+            B++;
+        }
+        if(C=='i' || C=='I'){
+            C++;
+        }
+        if(C=='o' || C=='O'){
+            D++;
+        }
+        if(C=='u' || C=='U'){
+            E++;
+        }
+    }
+    printf("%d %d %d %d %d", A, B, C, D, E);
+    return 0;
+}
